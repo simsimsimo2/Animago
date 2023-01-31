dee8ca7e4d85fbf1a618fb3ee2e22d64d8ddb24c
huylacite <81980443+huylacite@users.noreply.github.com>
2023-01-30 21:02:42 -0500

    Contact et histoire

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,11 +1,52 @@
 import { Inter } from '@next/font/google'
+import styles from '../styles/Contact.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Contact() {
-    return <>
-     <main>
-      <h1>HTTP 302 | Found Contact Page</h1>
-      <h1>The web page is under construction</h1>
-     </main>
-    </>
-  }
+  return <>
+    <main>
+      <div className={styles.bodyContact}>
+        <h1 className={styles.titreContact}>Rendez nous visite</h1>
+        <div className={styles.magasinContact}>
+          <div className={styles.adresseContact}>
+            <h2>Magasin Phare</h2>
+            <a>Tel: 1(800) 742-2483 </a>
+            <a>801 Aviation Pkwy,</a>
+            <a>Ottawa, ON K1K 4R3</a>
+            <div className={styles.adresseContact}>
+            <a className={styles.horaire}>Horaires d'ouvertures</a>
+            <a>Lun - Ven : 7h00 - 22h00 </a>
+            <a>Samedi : 8h - 22h</a>
+            <a>Dimanche : 8h - 23h</a>
+            </div>
+          </div>
+          <div className={styles.adresseContact}>
+            <h2>Animago! Ottawa</h2>
+            <a>Tel: 1-613-520-2600</a>
+            <a>1125 Colonel By Drive</a>
+            <a>Ottawa, ON K1S 5B6</a>
+            <div className={styles.adresseContact}>
+            <a className={styles.horaire}>Horaires d'ouvertures</a>
+            <a>Lun - Ven : 7h00 - 22h00 </a>
+            <a>Samedi : 8h - 22h</a>
+            <a>Dimanche : 8h - 23h</a>
+            </div>
+          </div>
+          <div className={styles.adresseContact}>
+            <h2>Animago! Toronto</h2>
+            <a>Tel: 1(800) 267-2483</a>
+            <a>9 Lower Javis st.</a>
+            <a>Toronto, ON M5E 0C3</a>
+            <div className={styles.adresseContact}>
+            <a className={styles.horaire}>Horaires d'ouvertures</a>
+            <a>Lun - Ven : 7h00 - 22h00 </a>
+            <a>Samedi : 8h - 22h</a>
+            <a>Dimanche : 8h - 23h</a>
+            </div>
+          </div>
+        </div>
+        <h1 className={styles.courriel}>Pour nous joindre : info@animago.com</h1>
+      </div>
+    </main>
+  </>
+}
--- pages/histoire.jsx
+++ pages/histoire.jsx
@@ -1,11 +1,20 @@
 import { Inter } from '@next/font/google'
+import styles from '../styles/Histoire.module.css'
+import Image from 'next/image';
+import ChienHistoire from '../public/img/ChienHistoire.png'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Histoire() {
-    return <>
-     <main>
-      <h1>HTTP 302 | Found Histoire Page</h1>
-      <h1>The web page is under construction</h1>
-     </main>
-    </>
+  return <>
+    <main>
+      <div className={styles.bodyHistoire}>
+        <h1 className={styles.titreHistoire}>Notre histoire</h1>
+        <div className={styles.histoire}>
+          <a>Après plusieurs heures de recherche pour trouver un manque service au niveau de la communauté, nous avons vu que le marché des articles pour animaux, en particulier les livraisons, sont très peu disponible.</a>
+          <a>L'équipe AnimaGo! est ravi de montrer leur vision de l'un des meilleurs futurs sites Web d'animaliers sur le marché, qui sera bientot présenté au monde.</a>
+        </div>
+        <Image src={ChienHistoire} alt='Un chien pour histoire' className={styles.imgResize} />
+      </div>
+    </main>
+  </>
   }
