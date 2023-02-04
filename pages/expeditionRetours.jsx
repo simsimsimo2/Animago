f3c897b838be933bf13dbe985292aaec89cdc690
huylacite <81980443+huylacite@users.noreply.github.com>
2023-02-04 18:41:35 -0500

    Page expedition et politique

--- pages/expeditionRetours.jsx
+++ pages/expeditionRetours.jsx
@@ -1,11 +1,24 @@
 import { Inter } from '@next/font/google'
+import styles from '../styles/Expedition.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Expedition() {
-    return <>
-     <main>
-      <h1>HTTP 302 | Found Expedition Page</h1>
-      <h1>The web page is under construction</h1>
-     </main>
-    </>
-  }
+  return <>
+    <main className={styles.bodyExpedition}>
+      <div className={styles.contenuExpedition}>
+        <h1 className={styles.titreExpedition}>Expédition & Retours</h1>
+        <a className={styles.soustitreExpedition}>Politique d'expédition</a>
+        <a className={styles.contenuExpedition}>Si nous connaissons un volume élevé de commandes, les expéditions peuvent être retardées de quelques jours. S'il vous plaît
+          prévoir des jours supplémentaires en transit pour la livraison. S'il y a un retard important dans l'expédition de votre
+          commande, nous vous contacterons par e-mail ou par téléphone.</a>
+
+        <a className={styles.soustitreExpedition}>Politique de retour et d'échange</a>
+        <a className={styles.contenuExpedition}>Les clients peuvent engager une procédure de retour dans les 7 jours à compter de la date de réception.
+          Si le client souhaite retourner ses achats, il est en droit de le faire sans nécessairement apporter de
+          justification. Le site marchand ne peut le refuser.</a>
+
+      </div>
+
+    </main>
+  </>
+}
--- pages/histoire.jsx
+++ pages/histoire.jsx
@@ -10,7 +10,7 @@ export default function Histoire() {
       <div className={styles.bodyHistoire}>
         <h1 className={styles.titreHistoire}>Notre histoire</h1>
         <div className={styles.histoire}>
-          <a>Après plusieurs heures de recherche pour trouver un manque service au niveau de la communauté, nous avons vu que le marché des articles pour animaux, en particulier les livraisons, sont très peu disponible.</a>
+          <a className={styles.contenu}>Après plusieurs heures de recherche pour trouver un manque service au niveau de la communauté, nous avons vu que le marché des articles pour animaux, en particulier les livraisons, sont très peu disponible.</a>
           <a>L'équipe AnimaGo! est ravi de montrer leur vision de l'un des meilleurs futurs sites Web d'animaliers sur le marché, qui sera bientot présenté au monde.</a>
         </div>
         <Image src={ChienHistoire} alt='Un chien pour histoire' className={styles.imgResize} />
