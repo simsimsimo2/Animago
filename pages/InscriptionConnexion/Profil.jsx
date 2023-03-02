df325bd8fe87331c03edf662b12ea77df3073151
Simon-Gab <cloutier.simongabriel@gmail.com>
2023-03-02 09:13:22 -0500

    Update Profil.jsx

--- pages/InscriptionConnexion/Profil.jsx
+++ pages/InscriptionConnexion/Profil.jsx
@@ -13,13 +13,18 @@ export default function Profil() {
     const [password, setPassword] = useState('');
     const [isLoggedin, setIsLoggedin] = useState(false);
     const users = [{ firstName: 'Bob', lastName: 'Gratton', email: "test@gmail.com", password: "test" }];
+    const [items, setItems] = useState([]);
     
     useEffect(() => {
         if(localStorage.getItem('token-info') !== null){
-            console.log(localStorage.getItem('token-info'));
+            const items = JSON.parse(localStorage.getItem('token-info'));
+            if (items) {
+                setItems(items);
+            }
         }
      }, []);
-    console.log('test afficahge nom',users.firstName)
+
+
     return <>
      <main>
         <div className={styles.container}>
@@ -27,7 +32,7 @@ export default function Profil() {
                 <button className={styles.button} onClick={() => router.push('/Accueil')}>← Aller à l'accueil</button>
             </div>
             <div className={styles.title}>
-                <h2>Bonjour </h2> <h2>{firstName}</h2>
+                <h2>Bonjour </h2> <h2>{items.firstName} {items.lastName}</h2>
               </div>
         </div>
      </main>
